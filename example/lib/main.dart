7386a12aaf8211d61b5a139b2f7240a3a1fcac38
Abdul Wahab Khan <awkhan313@gmail.com>
2025-02-05 00:42:18 +0500

    adding the main class

---
+++ example/lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:example/data.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_sketchy_text/flutter_sketchy_text.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Sketchy Text Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Sketchy Text Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SketchyParagraph(
+                paragraph: paragraphModel.paragraph,
+                highlights: paragraphModel.highlightSentances),
+          ],
+        ),
+      ),
+    );
+  }
+}
